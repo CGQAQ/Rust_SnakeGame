11ba96bcd68ef58e0089fe827845d23ec896486d
Jason <jason@localhost.localdomain>
2018-07-02 21:53:26 +0800

    init

--- src/main.rs
+++ src/main.rs
@@ -13,13 +13,13 @@ use glutin_window::GlutinWindow as Window;
 use opengl_graphics::{ GlGraphics, OpenGL };
 
 
-const UNIT: f64 = 20f64;
-const WIDTH: u32 = 700;
-const HEIGHT: u32 = 500;
-const THICKNESS: u32 = 20;
+const UNIT: u32 = 20;
+const WIDTH: u32 = UNIT * 30;
+const HEIGHT: u32 = UNIT * 20;
+const THICKNESS: u32 = UNIT;
 
 
-#[derive(Debug, Clone)]
+#[derive(Debug, Clone, Eq, PartialEq)]
 struct Point{
     x: u32,
     y: u32,
@@ -50,6 +50,9 @@ pub struct Game {
     state: State,
 
     food: Point,
+
+    can_turn: bool,
+    inited: bool,
     //wall: [Point; 4]
 }
 
@@ -64,32 +67,40 @@ impl Game {
 
             body.iter().for_each(|p: &Point| {
 //                println!("{:?}", p);
-                let body = rectangle::square(p.x as f64, p.y as f64, UNIT);
-                rectangle(color::BLACK, body, c.transform, gl);
+                let body_square = rectangle::square(p.x as f64, p.y as f64, UNIT as f64);
+                if p == body.front().unwrap(){
+                    rectangle(color::BLACK, body_square, c.transform, gl);
+                }
+                else {
+                    rectangle(color::hex("FFFF00"), body_square, c.transform, gl);
+                }
             });
         });
     }
 
     fn update(&mut self, args: &UpdateArgs) {
         if self.state == State::Started {
+            self.inited = false;
+
             let body = &mut self.body;
             let dir = &self.dir;
             let mut last = Point{x:0, y:0};
 
             if body.len() == 1 {
-                last = body.pop_front().unwrap();
+                last = body.pop_back().unwrap();
             }
-                else if body.len() > 1{
-                    last = (*body.back().unwrap()).clone();
-                }
+            else if body.len() > 1{
+                body.pop_back();
+                last = (*body.front().unwrap()).clone();
+            }
 
             let p = match *dir {
-                Direction::Up => Point{x: last.x, y: last.y - 1},
-                Direction::Down => Point{x: last.x, y: last.y + 1},
-                Direction::Left => Point{x: last.x - 1, y: last.y},
-                Direction::Right => Point{x: last.x + 1, y: last.y},
+                Direction::Up => Point{x: last.x, y: last.y - UNIT},
+                Direction::Down => Point{x: last.x, y: last.y + UNIT},
+                Direction::Left => Point{x: last.x - UNIT, y: last.y},
+                Direction::Right => Point{x: last.x + UNIT, y: last.y},
             };
-            body.push_back(p);
+            body.push_front(p);
         }
     }
 
@@ -113,33 +124,43 @@ impl Game {
 
     fn hit(&mut self) -> bool{
 //        self.  碰撞检测！！
-
-        true
+        let head = self.body.front().unwrap();
+        if head.x < THICKNESS || head.x > WIDTH - (THICKNESS+UNIT) || head.y < THICKNESS || head.y > HEIGHT - (THICKNESS+UNIT){
+            true
+        }
+        else {
+            false
+        }
     }
 
 
     #[allow(non_snake_case)]
     fn button_pressed(&mut self, args: Key){
+        let can_turn = &mut self.can_turn;
         match args{
             // Snake control
             Key::Up => {
-                if self.dir != Direction::Down {
+                if self.dir != Direction::Down && *can_turn == true {
                     self.dir = Direction::Up;
+                    *can_turn = false;
                 }
             },
             Key::Down => {
-                if self.dir != Direction::Up {
+                if self.dir != Direction::Up && *can_turn == true  {
                     self.dir = Direction::Down;
+                    *can_turn = false
                 }
             },
             Key::Left => {
-                if self.dir != Direction::Right{
+                if self.dir != Direction::Right && *can_turn == true {
                     self.dir = Direction::Left;
+                    *can_turn = false
                 }
             },
             Key::Right => {
-                if self.dir != Direction::Left {
+                if self.dir != Direction::Left  && *can_turn == true {
                     self.dir = Direction::Right;
+                    *can_turn = false
                 }
             },
 
@@ -166,8 +187,13 @@ impl Game {
 
     fn init(&mut self) {
         // reinit the game!
+        self.state = State::Stopped;
         self.body.clear();
-        self.body.push_back(Point{x:50, y:50})
+        self.body.push_back(Point{x:UNIT*5, y:UNIT*5});
+        self.body.push_back(Point{x:UNIT*4, y:UNIT*5});
+        self.body.push_back(Point{x:UNIT*3, y:UNIT*5});
+        self.can_turn = true;
+        self.inited = true;
     }
 
 
@@ -195,21 +221,33 @@ fn main() {
         body: LinkedList::<Point>::new(),
         state: State::Stopped,
         food: Point{x: 0, y: 0},
+
+        can_turn: true,
+        inited: true,
     };
 
 
 
     game.init();
 
-    let mut events = Events::new(EventSettings::new());
+    let mut events = Events::new(EventSettings::new()).ups(2u64);
     while let Some(e) = events.next(&mut window) {
         if let Some(r) = e.render_args() {
+//            println!("{:?}", game.can_turn);
             game.draw_wall(&r);
             game.render(&r);
+            if game.hit(){
+                game.init();
+            }
+
+            if game.state == State::Stopped && game.inited == false{
+                game.init();
+            }
         }
 
         if let Some(u) = e.update_args() {
             game.update(&u);
+            game.can_turn = true;
         }
 
         if let Some(Button::Keyboard(key)) = e.press_args(){
